3fc96cbc40fb925ba352ade345a5629b75d2b77c
Mathushan Gnanasegaram <mathushan.g@macbook-air.home>
2025-11-27 18:39:16 -0500

    Update homepage hero video to latest Cloudinary video

--- lib/videoConfig.ts
+++ lib/videoConfig.ts
@@ -1,7 +1,7 @@
 // lib/videoConfig.ts
 
 export const videoConfig = {
-  hero: "https://res.cloudinary.com/drbh1hki1/video/upload/v1764285723/Untitled_design_u4vdjj.mp4",
+  hero: "https://res.cloudinary.com/drbh1hki1/video/upload/v1764286665/Untitled_design_1_v7pylg.mp4",
   stayYoutube: "https://www.youtube.com/embed/P4GQP5FozYo?rel=0&modestbranding=1&playsinline=1",
 };
 
